c49959df6c1e043564f6c1d088b42b7848d374d8
Sobaka010 <154821512+Sobaka010@users.noreply.github.com>
2025-03-07 12:13:23 +0500

    Update task 1.fs

--- lab 2/task 1.fs
+++ lab 2/task 1.fs
@@ -1,12 +1,17 @@
-﻿open System
+open System
 
-let rec schitivanie x =
-    match string(Console.ReadLine()) with
-    | "" -> List.rev x
-    | y -> schitivanie (y :: x) 
+let rec schitivanie n =
+    if n <= 0 then
+        []
+    else
+        let y = string(Console.ReadLine())
+        y :: schitivanie (n-1) 
 
+
+printf "Введите кол-во строк в списке: "
+let n = int (Console.ReadLine())
 printfn "Введите строку через Enter, для завершения введите пустой элемент: "
-let list1 = schitivanie []
+let list1 = (schitivanie n)
 printfn "Введённый список: %A" list1
 
 let list2 = List.map String.length list1
