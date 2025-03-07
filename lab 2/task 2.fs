ae181321e5f7c203f56a8a9509a956c26e71ff82
Sobaka010 <154821512+Sobaka010@users.noreply.github.com>
2025-03-07 12:12:53 +0500

    Update task 2.fs

--- lab 2/task 2.fs
+++ lab 2/task 2.fs
@@ -1,40 +1,40 @@
-﻿open System
 
-let rec proverka x y = // х - число, y - цифра
-    let x = abs x
-    if x = 0 then
-        false
-    else
-        if x % 10 = y then
-            true  
-        else
-            proverka (x / 10) y
-
-let rec generator n min max =
-    if n <= 0 then
-        []  
-    else
-        let random = new Random()
-        let number = random.Next(min, max) 
-        number :: (generator (n - 1) min max)
-
-let sum list y = // х - число, y - цифра
+let sum list y = // y - цифра
     List.fold (fun acc x2 -> 
         if proverka x2 y then // х2 - число, в котором есть y
             acc + x2  
         else
             acc) 0 list  
 
-printf "Введите кол-во элементов в случайном списке: "
-let n = int(Console.ReadLine())
-printf "Введите МИНИМАЛЬНОЕ значение элемента в списе:  "
-let min = int(Console.ReadLine())
-printf "Введите МАКСИМАЛЬНОЕ значение элемента в списе: "
-let max = int(Console.ReadLine())
-let list = (generator n min max)
-printf "Случайный список: %A" list
-printf "\nВведите цифру: "
-let y = int(Console.ReadLine())
 
-let s = (sum list y)
-printf "Сумма всех элементов, содержащих эту цифру: %A\n\n" s
+printfn "Выберите способ заполнения списка:"
+printfn "1 - Случайными числами"
+printfn "2 - Вручную"
+match Console.ReadLine() with
+| "1" ->
+    printf "Введите кол-во элементов в случайном списке: "
+    let n = int(Console.ReadLine())
+    printf "Введите МИНИМАЛЬНОЕ значение элемента в списе:  "
+    let min = int(Console.ReadLine())
+    printf "Введите МАКСИМАЛЬНОЕ значение элемента в списе: "
+    let max = int(Console.ReadLine())
+    let list = (generator_r n min max)
+    printf "Случайный список: %A" list
+    printf "\nВведите цифру: "
+    let y = int(Console.ReadLine())
+
+    let s = (sum list y)
+    printf "Сумма всех элементов, содержащих эту цифру: %A\n\n" s
+
+| "2" ->
+    printf "Введите кол-во элементов в случайном списке: "
+    let n = int(Console.ReadLine())
+    let list = (generator_k n)
+    printf "Ваш список: %A" list
+    printf "\nВведите цифру: "
+    let y = int(Console.ReadLine())
+
+    let s = (sum list y)
+    printf "Сумма всех элементов, содержащих эту цифру: %A\n\n" s
+
+| _ -> printfn "Некорректный ввод, попробуйте снова."
